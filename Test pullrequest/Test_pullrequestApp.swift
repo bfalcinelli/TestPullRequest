ad0a42d34571b7d8318a49fd499c2dff049b4f4b
Boris Falcinelli <bfalcinelli@technogym.com>
2023-06-09 16:54:24 +0200

    first commit

---
+++ Test pullrequest/Test_pullrequestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_pullrequestApp.swift
+//  Test pullrequest
+//
+//  Created by Falcinelli Boris - Technogym on 09/06/23.
+//
+
+import SwiftUI
+
+@main
+struct Test_pullrequestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
